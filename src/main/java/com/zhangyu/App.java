0263054cd824a950be31f044884071a7760cccdd
yuzhang <1580074674@qq.com>
2015-12-13 14:30:56 +0800

    [zhangyu] cross request without security

--- src/main/java/com/zhangyu/App.java
+++ src/main/java/com/zhangyu/App.java
@@ -1,13 +1,11 @@
 package com.zhangyu;
 
 import com.zhangyu.filter.RequestLoggingFilter;
-import com.zhangyu.interceptor.CustomerInterceptor;
 import com.zhangyu.listener.CustomerListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @SpringBootApplication
 public class App {
@@ -16,11 +14,6 @@ public class App {
         SpringApplication springApplication = new SpringApplication(App.class);
         springApplication.addListeners(new CustomerListener());
         springApplication.run(args);
-    }
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new CustomerInterceptor())
-                .addPathPatterns("/customers/**");
     }
 
     @Bean
---
+++ src/main/java/com/zhangyu/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.zhangyu.config;
+
+import com.zhangyu.interceptor.CustomerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CustomerInterceptor())
+                .addPathPatterns("/customers/**");
+    }
+}
+
